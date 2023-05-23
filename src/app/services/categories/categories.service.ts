98a435441e8cf1eb0c1ee04ced8646566272e6c1
Nguyen Trung Duc <huyga612@gmail.com>
2023-05-23 15:36:00 +0700

    add categories services

--- src/app/services/categories/categories.service.ts
+++ src/app/services/categories/categories.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8000/api/categories'
+  constructor(private http: HttpClient) { }
+
+
+  getAllCategories(){
+    return this.http.get(this.apiUrl)
+  }
+
+  getOneCategory(id:number|string){
+    return this.http.get(this.apiUrl + `/${id}`)
+
+  }
 }
